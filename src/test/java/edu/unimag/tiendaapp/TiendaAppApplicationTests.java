918917b24d2228c36cc4fc2bfd5f987f7501ed8f
Cesar David Fawcett Perozo <31809162+CesarFawcett@users.noreply.github.com>
2025-04-04 12:42:57 -0500

    Add files via upload

---
+++ src/test/java/edu/unimag/tiendaapp/TiendaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unimag.tiendaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
